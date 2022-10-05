0b6c812801dc725b2537034c243251bdfa892e60
Ami-OS <grizzltty.app@gmail.com>
2022-10-05 21:03:23 +0800

    Fix context menu position

--- source/Components/ContextMenu/ContextMenu.js
+++ source/Components/ContextMenu/ContextMenu.js
@@ -34,24 +34,56 @@ class ContextMenu extends Menu {
   show(x, y) {
     super.show();
 
+    this.el_.append.style.width = '';
+    this.el_.append.style.height = '';
+    this.el_.style.width = '';
+    this.el_.style.height = '';
+
+    const rect = this.player_.el().getBoundingClientRect();
+    const { width, height } = this.el_.getBoundingClientRect();
+
+    const posX = rect.width - width;
+    const posY = rect.height - height;
+
+    let calcX = 0;
+    let calcY = 0;
+
+    if (x > posX) {
+      calcX = (x - posX) * -1;
+    }
+
+    if (y > posY) {
+      calcY = (y - posY) * -1;
+    }
+
+    const marginTop = calcY;
+    const marginLeft = calcX;
+
     this.el_.style.top = y + 'px';
     this.el_.style.left = x + 'px';
+    this.el_.style.marginTop = marginTop + 'px';
+    this.el_.style.marginLeft = marginLeft + 'px';
+
+    this.el_.append.style.width = width + 'px';
+    this.el_.append.style.height = height + 'px';
+    this.el_.style.width = width + 'px';
+    this.el_.style.height = height + 'px';
   }
 
   onContextmenu(event) {
     event.preventDefault();
 
     const rect = this.player_.el().getBoundingClientRect();
-    const { pageX, pageY } = event;
+    const { clientX, clientY } = event;
 
     if (
-      pageY > rect.y &&
-      pageY - rect.height < rect.y &&
-      pageX > rect.x &&
-      pageX - rect.width < rect.x
+      clientY > rect.y &&
+      clientY - rect.height < rect.y &&
+      clientX > rect.x &&
+      clientX - rect.width < rect.x
     ) {
-      const x = pageX - rect.x;
-      const y = pageY - rect.y;
+      const x = clientX - rect.x;
+      const y = clientY - rect.y;
 
       this.show(x, y);
     } else {
