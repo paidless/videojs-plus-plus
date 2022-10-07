ae714a6aa985e29f9e3268e7a1ec3dc62fea1042
Ami-OS <grizzltty.app@gmail.com>
2022-10-08 01:31:31 +0800

    Add live dvr switch

--- source/Plugin/Live/Live.js
+++ source/Plugin/Live/Live.js
@@ -4,9 +4,15 @@ import './Live.scss';
 
 const Plugin = videojs.getPlugin('plugin');
 
+const defaults = {
+  dvr: true
+}
+
 class Live extends Plugin {
   constructor(player, options = {}) {
     super(player, options);
+
+    this.options = videojs.mergeOptions(defaults, options);
 
     this.createLiveNotive(player);
 
@@ -28,11 +34,15 @@ class Live extends Plugin {
     const onTimeupdate = this.onTimeUpdate.bind(this);
 
     player.addClass('vjs-live-streaming');
+    if (this.options.dvr) {
+      player.addClass('vjs-live-dvr');
+    }
     player.on('timeupdate', onTimeupdate);
 
     this.on('dispose', () => {
       player.off('timeupdate', onTimeupdate);
       player.removeClass('vjs-live-streaming');
+      player.removeClass('vjs-live-dvr');
       player.removeClass('vjs-live');
     });
   }
